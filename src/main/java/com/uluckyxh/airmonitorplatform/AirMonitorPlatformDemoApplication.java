56dfee4dd6f7b4e3d46215c35db6e3aa148cb0e6
XiaoHuang <1670884979@qq.com>
2025-02-12 21:06:06 +0800

    one first

---
+++ src/main/java/com/uluckyxh/airmonitorplatform/AirMonitorPlatformDemoApplication.java
@@ -0,0 +1,13 @@
+package com.uluckyxh.airmonitorplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirMonitorPlatformDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirMonitorPlatformDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uluckyxh/airmonitorplatform/AirMonitorPlatformDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uluckyxh.airmonitorplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirMonitorPlatformDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
